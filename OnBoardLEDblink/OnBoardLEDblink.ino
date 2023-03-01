daee3ea797bb682389c7480d9863103924a24a1e
Matthew Clifford <mhmcliff@gmail.com>
2023-03-01 14:07:06 -0800

    Added LED delay

--- OnBoardLEDblink/OnBoardLEDblink.ino
+++ OnBoardLEDblink/OnBoardLEDblink.ino
@@ -35,7 +35,12 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+  delay(500);                       // wait for a half second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  delay(250);                       // wait for a quarter second
+  
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);                       // wait for 2 seconds
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1500);                       // wait for 1.5 seconds/ 
 }
